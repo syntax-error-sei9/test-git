877ab324d54576a6b964210b36ad997a4b2258c0
gneral20 <gneral20@gmail.com>
2020-01-09 16:07:03 +0300

    I added meshal file to the project with class

---
+++ meshal.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Meshal extends Component {
+    state = {  }
+    render() { 
+        return ( 
+        <div>
+            <h1>Hi I am Meshal</h1>
+            <h2>What is your name?</h2>
+        </div>
+         );
+    }
+}
+ 
+export default Meshal;
